b111552fe4238d312300898a340a10fbf0ce0a9e
ahslxjvhapt12 <likjhee1@gmail.com>
2023-12-27 20:29:24 +0900

    Move

--- Assets/02_Script/StageSelectPlayer.cs
+++ Assets/02_Script/StageSelectPlayer.cs
@@ -1,14 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class StageSelectPlayer : MonoBehaviour
 {
+    Camera _mainCam;
+
+    private float _xRotate, _yRotate;
+    public float _rotateSpeed = 500.0f;
+
+    private void Awake()
+    {
+        _mainCam = Camera.main;
+    }
+
     private void Update()
     {
-        float h = Input.GetAxis("Horizontal");
-        float v = Input.GetAxis("Vertical");
+        // 이동
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
 
-        transform.position += new Vector3(h, 0, v).normalized;
+        transform.position += (_mainCam.transform.right * h + _mainCam.transform.forward * v).normalized * Time.deltaTime;
+
+
+        // 회전
+        float x = -Input.GetAxis("Mouse Y") * Time.deltaTime * _rotateSpeed;
+        float y = Input.GetAxis("Mouse X") * Time.deltaTime * _rotateSpeed;
+
+        _yRotate = transform.eulerAngles.y + y;
+        //xRotate = transform.eulerAngles.x + xRotateMove; 
+        _xRotate = _xRotate + x;
+
+        _xRotate = Mathf.Clamp(_xRotate, -90, 90); // 위, 아래 고정
+
+        transform.eulerAngles = new Vector3(_xRotate, _yRotate, 0);
     }
 }
