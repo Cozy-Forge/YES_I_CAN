0e2b1cbeac7823154c83d09ee338f657d58079eb
준1 <98933515+Wizone06@users.noreply.github.com>
2023-12-28 11:22:18 +0900

    dad
    asdda

--- Assets/02_Script/Can/CamFollow.cs
+++ Assets/02_Script/Can/CamFollow.cs
@@ -2,17 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CanFollow : MonoBehaviour
+public class CamFollow : MonoBehaviour
 {
+    [SerializeField]
+    private GameObject Can;
     // Start is called before the first frame update
     void Start()
     {
-        
+        Can = GameObject.Find("CAN");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.position = Can.transform.position;
     }
 }
--- Assets/02_Script/Can/CameraRotate.cs
+++ Assets/02_Script/Can/CameraRotate.cs
@@ -4,27 +4,10 @@ using UnityEngine;
 
 public class CameraRotate : MonoBehaviour
 {
-    public Camera cam;
+    
 
-    private void Awake()
+    void LateUpdate()
     {
-        cam = GetComponentInChildren<Camera>();
-    }
-
-    private void Update()
-    {
-        LookAtMouse();
-    }
-
-    public void LookAtMouse()
-    {
-        Ray ray = cam.ScreenPointToRay(Input.mousePosition);
-        RaycastHit hit;
-
-        if (Physics.Raycast(ray, out hit))
-        {
-            Vector3 lookPos = new Vector3(hit.point.x, transform.position.y, hit.point.z);
-            transform.LookAt(lookPos);
-        }
+       //1ÀÎÄª °¬´Ù¿Í¼­ ¸¸µé±â
     }
 }
