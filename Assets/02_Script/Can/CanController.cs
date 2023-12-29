3ee89ca349a89ae4a2ace94b36752256c9aeddd3
준1 <98933515+Wizone06@users.noreply.github.com>
2023-12-29 11:11:07 +0900

    텍스트로 쿨타임 표시
    텍스트로 쿨타임 표시

--- Assets/02_Script/Can/CanController.cs
+++ Assets/02_Script/Can/CanController.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UIElements;
 
@@ -13,26 +14,37 @@ public class CanController : MonoBehaviour
     public float JumpCool;
     public GameObject JumpEffect;
     public Transform EffectPos;
+    public TMP_Text JumpTMP;
 
     void Awake()
     {
+        JumpTMP.enabled = false;
         cam = Camera.main;
         rb = GetComponent<Rigidbody>();
+        JumpTMP = GetComponentInChildren<TextMeshPro>();  
     }
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space) && JumpCool >=2)
+        if (Input.GetKeyDown(KeyCode.Space) && JumpCool <= 0)
         {
             Vector3 forwardDirection = cam.transform.forward + Vector3.up;
             rb.AddForce(forwardDirection * ForcePower, ForceMode.VelocityChange);
 
             rb.velocity = Vector3.ClampMagnitude(rb.velocity, 10);
-            JumpCool = 0;
+            JumpCool = 2;
             Instantiate(JumpEffect, EffectPos.position, Quaternion.identity);
+        }else if(JumpCool > 0)
+        {
+            JumpTMP.enabled = true;
+            JumpCool -= Time.deltaTime;
+            JumpTMP.text = JumpCool.ToString("0");
+            if(JumpCool <= 0)
+            {
+                JumpTMP.enabled = false;
+            }
         }
 
-        JumpCool += Time.deltaTime;
 
     }
 
